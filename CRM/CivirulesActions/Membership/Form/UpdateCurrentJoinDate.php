53585a46f77f0c3cc7900253efb425e38e46eb19
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2020-06-22 15:51:59 +0100

    Rules Updates for - 1. Reject, 2. Extra Tags

--- CRM/CivirulesActions/Membership/Form/UpdateCurrentJoinDate.php
+++ CRM/CivirulesActions/Membership/Form/UpdateCurrentJoinDate.php
@@ -11,7 +11,22 @@ class CRM_CivirulesActions_Membership_Form_UpdateCurrentJoinDate extends CRM_Civ
   public function buildQuickForm() {
     $this->add('hidden', 'rule_action_id');
     $membershipTypes = CRM_Member_PseudoConstant::membershipType();
-    $this->add('select', 'membership_type_id', ts('Membership Type to Action On'), array('' => ts('-- please select --')) + $membershipTypes, TRUE);
+    $attributes = ['multiple' => 'multiple', 'class' => 'crm-select2 huge', 'placeholder' => ts('Membership Types')];
+    $this->add('select', 'membership_type_ids', ts('Membership Type to Action On'), $membershipTypes, TRUE, $attributes);
+
+    $options = [
+      1 => ts('Set'),
+      0 => ts('Remove'),
+    ];
+    $this->addRadio('is_override', ts('Override Flag'), $options, ['allowClear' => TRUE], NULL, TRUE);
+
+    $options = ['' => ts('-- please select --')] + CRM_Member_PseudoConstant::membershipStatus(NULL, NULL, 'label');
+    $this->add('select', 'membership_status_to', ts('Change Membership Status To'), $options);
+
+    $this->add('select', 'membership_type_tags', ts('Tag to Apply'), CRM_Core_BAO_Tag::getTags());
+
+    $attributes['placeholder'] = ts('Membership Types to Tag');
+    $this->add('select', 'tag_membership_type_ids', ts('Tag Membership Type Condition'), $membershipTypes, FALSE, $attributes);
 
     $this->addButtons(array(
       array('type' => 'next', 'name' => ts('Save'), 'isDefault' => TRUE,),
--- CRM/CivirulesActions/Membership/UpdateCurrentJoinDate.php
+++ CRM/CivirulesActions/Membership/UpdateCurrentJoinDate.php
@@ -40,40 +40,66 @@ class CRM_CivirulesActions_Membership_UpdateCurrentJoinDate extends CRM_Civirule
     $actionParams = $this->getActionParameters();
     $contactId = $triggerData->getContactId();
 
-    $getResult = civicrm_api3('Membership', 'get', [
-      'sequential' => 1,
-      'contact_id' => $contactId,
-      'membership_type_id' => $actionParams['membership_type_id'],
-      'status_id' => "New",//fixme: setting
-    ]);
-    if (!empty($getResult['id'])) {
-      // dates based on today
-      $memTypeDates = CRM_Member_BAO_MembershipType::getDatesForMembershipType($actionParams['membership_type_id']);
+    foreach ($actionParams['membership_type_ids'] as $memTypeId) {
+      $getResult = civicrm_api3('Membership', 'get', [
+        'sequential' => 1,
+        'contact_id' => $contactId,
+        'membership_type_id' => $memTypeId,
+        'status_id' => "New",//fixme: setting
+      ]);
+      if (!empty($getResult['id'])) {
+        // dates based on today
+        $memTypeDates = CRM_Member_BAO_MembershipType::getDatesForMembershipType($memTypeId);
 
-      // Convert all dates to 'Y-m-d' format.
-      foreach (['join_date', 'start_date', 'end_date'] as $dateParam) {
-        if (!empty($memTypeDates[$dateParam])) {
-          $$dateParam = CRM_Utils_Date::processDate($memTypeDates[$dateParam], NULL, FALSE, 'Y-m-d');
+        // Convert all dates to 'Y-m-d' format.
+        foreach (['join_date', 'start_date', 'end_date'] as $dateParam) {
+          if (!empty($memTypeDates[$dateParam])) {
+            $$dateParam = CRM_Utils_Date::processDate($memTypeDates[$dateParam], NULL, FALSE, 'Y-m-d');
+          }
+        }
+        //if (!$joinDate) {
+        //  $joinDate = date('Y-m-d');
+        //}
+        $join_date = $getResult['values'][0]['join_date'];
+        if ($join_date == date('Y-m-d')) {
+          $join_date = date("Y-m-d", strtotime("yesterday"));
+        }
+        $params = [];
+        if (empty($actionParams['is_override'])) {
+          $params = [
+            'membership_type_id' => $memTypeId,
+            'id'            => $getResult['id'],
+            'contact_id'    => $contactId,
+            'join_date'     => $join_date,
+            'start_date'    => $start_date,
+            'end_date'      => $end_date,
+            'is_override'   => 0,
+            'skipStatusCal' => 0,
+          ];
+        } else if ($actionParams['membership_status_to']) {
+          $params = [
+            'membership_type_id' => $memTypeId,
+            'id'            => $getResult['id'],
+            'contact_id'    => $contactId,
+            'is_override'   => 1,
+            'skipStatusCal' => 1,
+            'status_id'     => $actionParams['membership_status_to'],
+          ];
+        }
+        if (!empty($params)) {
+          $updateResult = civicrm_api3('Membership', 'create', $params);
+        }
+
+        if (in_array($memTypeId, $actionParams['tag_membership_type_ids']) && 
+          !empty($actionParams['membership_type_tags'])
+        ) {
+          $params = [
+            'contact_id' => $contactId,
+            'tag_id' => $actionParams['membership_type_tags'],
+          ];
+          $result = civicrm_api3('EntityTag', 'create', $params);
         }
       }
-      //if (!$joinDate) {
-      //  $joinDate = date('Y-m-d');
-      //}
-      $join_date = $getResult['values'][0]['join_date'];
-      if ($join_date == date('Y-m-d')) {
-        $join_date = date("Y-m-d", strtotime("yesterday"));
-      }
-      $params = [
-        'membership_type_id' => $actionParams['membership_type_id'],
-        'id'            => $getResult['id'],
-        'contact_id'    => $contactId,
-        'join_date'     => $join_date,
-        'start_date'    => $start_date,
-        'end_date'      => $end_date,
-        'is_override'   => 0,
-        'skipStatusCal' => 0,
-      ];
-      $updateResult = civicrm_api3('Membership', 'create', $params);
     }
   }
 
